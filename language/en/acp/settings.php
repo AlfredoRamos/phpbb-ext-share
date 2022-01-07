3acc2d7bb1f019e0345bad1c4f78b0a3aa05b810
Alfredo Ramos <alfredo.ramos.sanchez@gmail.com>
2022-01-07 14:47:31 -0600

    Add notice for drag and drop functionality

--- language/en/acp/settings.php
+++ language/en/acp/settings.php
@@ -25,12 +25,13 @@ if (empty($lang) || !is_array($lang))
 
 $lang = array_merge($lang, [
 	'ACP_SHARE_SETTINGS' => 'Share Links settings',
-	'ACP_SHARE_SETTINGS_EXPLAIN' => 'Here you can choose the social networks you want to show share links for.',
+	'ACP_SHARE_SETTINGS_EXPLAIN' => 'Here you can choose which social networks you want to show when sharing posts.',
 	'ACP_SHARE_SETTINGS_WARNING' => 'You have unsaved changes, if you leave this page your current changes will be lost.',
 	'ACP_SHARE_TYPE' => 'Type',
 	'ACP_SHARE_TYPE_EXPLAIN' => 'The type determines the location of the sharing links.',
 	'ACP_SHARE_TOPIC' => 'Topic',
 	'ACP_SHARE_POST' => 'Post',
 	'ACP_SHARE_SOCIAL_NETWORKS' => 'Social Networks',
+	'ACP_SHARE_PREVIEW_EXPLAIN' => 'Drag and drop to change the order of the social networks.',
 	'ACP_SHARE_VALIDATE_VALUES_NOT_ALLOWED' => 'The values given for <samp>%1$s</samp> are not allowed: <code>%2$s</code>'
 ]);
